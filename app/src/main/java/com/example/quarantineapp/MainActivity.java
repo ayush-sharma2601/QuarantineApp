8956a9931293ccf0255145298998ab10349bbb1f
ayush-sharma2601 <ayushsh2601@gmail.com>
2020-03-29 19:26:18 +0530

    added login/signup activities

--- app/src/main/java/com/example/quarantineapp/MainActivity.java
+++ app/src/main/java/com/example/quarantineapp/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
@@ -116,5 +117,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void DrawerClose(MenuItem item) {
         drawerLayout.closeDrawer(Gravity.LEFT);
+    }
+
+    public void ShopLogin(MenuItem item) {
+        Intent goShop = new Intent(MainActivity.this,Login)
+    }
+
+    public void HospLogin(MenuItem item) {
     }
 }
