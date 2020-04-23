efe20a41c9bc577560d04048bc2a7b8372d66ddf
ayush-sharma2601 <ayushsh2601@gmail.com>
2020-04-23 14:46:01 +0530

    Added donation link

--- app/src/main/java/com/example/quarantineapp/DonationFragment.java
+++ app/src/main/java/com/example/quarantineapp/DonationFragment.java
@@ -1,5 +1,7 @@
 package com.example.quarantineapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,24 +10,24 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class DonationFragment extends Fragment {
     public DonationFragment() {
     }
     View view;//change names of views in different fragment classes if needed for clarity
-    TextView donateBtn;
+    CardView donateBtn;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.donation_fragment,container,false);
         donateBtn = view.findViewById(R.id.donate_btn);
-        donateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //open site to donate to some organization
-            }
+        donateBtn.setOnClickListener(v -> {
+            //open site to donate to some organization
+            Intent donate = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.akshayapatra.org/covid-relief-services"));
+            startActivity(donate);
         });
         return view;
 
